5d42f30c440674fed01986365b7ff60209b8a4f0
alpylmz <ylmz.alp.e@gmail.com>
2022-05-01 01:19:19 +0300

    Feature: Board elements are added to frontend

--- src/main/java/group2/monopoly/frontend/BoardRectangles.java
+++ src/main/java/group2/monopoly/frontend/BoardRectangles.java
@@ -17,20 +17,23 @@ public class BoardRectangles {
     int width, height;
     int text_margin_width = 20;
     int text_margin_height = 30;
+    List<String> board_elements;
 
-    public BoardRectangles(int width, int height) {
+    public BoardRectangles(int width, int height, List<String> board_elements) {
         rects = new Group();
         this.width = width;
         this.height = height;
+        this.board_elements = board_elements;
     }
 
-    public void CreateRectangles() {
+    public void createRectangles() {
         // There is a 5x5 board that we will fit into the whole screen
         int i = 0;
         int j = 0;
         int width_step = width / 7;
         int height_step = height / 7;
         ArrayList<StackPane> stackPanes = new ArrayList<>();
+        int ctr = 0;
         for (i = 0; i < 5; i++) {
             for (j = 0; j < 5; j++) {
                 if (i == 0 || j == 0 || i == 4 || j == 4) {
@@ -38,7 +41,7 @@ public class BoardRectangles {
                     rect.setFill(Color.CORNFLOWERBLUE);
                     rect.setStroke(Color.BLACK);
                     StackPane stack = new StackPane();
-                    Text text = new Text("Start point");
+                    Text text = new Text(board_elements.get(ctr));
                     text.setStyle("-fx-font: 20 arial; -fx-text-fill: red;");
                     //text.setX(width_step * (i + 1) + text_margin_width);
                     //text.setY(height_step * (j + 1) + text_margin_height);
@@ -53,6 +56,7 @@ public class BoardRectangles {
 
                     rects.getChildren().add(stack);
                     System.out.println("Rectangle added: " + i + " " + j);
+                    ctr++;
 
                 }
             }
--- src/main/java/group2/monopoly/frontend/HelloApplication.java
+++ src/main/java/group2/monopoly/frontend/HelloApplication.java
@@ -6,14 +6,40 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         int width = 800;
         int height = 600;
-        BoardRectangles board_rectangles = new BoardRectangles(width, height);
-        board_rectangles.CreateRectangles();
+        List<String> places = new ArrayList<>();
+        // income tax random
+        // railroad/ferry 4 random
+        // 8 properties random
+
+        // mock
+        places.add("Jail/Just \nvisiting"); // top left
+        places.add("Income tax");
+        places.add("Railroad/\nferry");
+        places.add("Railroad/\nferry");
+        places.add("Start point"); // bottom left
+        places.add("Railroad/\nferry");
+        places.add("Railroad/\nferry");
+        places.add("Property\nx$");
+        places.add("Property\nx$");
+        places.add("Property\nx$");
+        places.add("Property\nx$");
+        places.add("Property\nx$");
+        places.add("Property\nx$");
+        places.add("Property\nx$");
+        places.add("Property\nx$");
+        places.add("Go to jail"); // bottom right
+
+
+        BoardRectangles board_rectangles = new BoardRectangles(width, height, places);
+        board_rectangles.createRectangles();
         Scene scene = new Scene(board_rectangles.getRects(), width, height);
         stage.setTitle("Monopoly");
         stage.setScene(scene);
