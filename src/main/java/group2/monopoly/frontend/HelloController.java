6e6dac5cf087e8241631cbb516bea06b7e409bf8
alpylmz <ylmz.alp.e@gmail.com>
2022-04-30 14:19:56 +0300

    Project Initialization

---
+++ src/main/java/group2/monopoly/frontend/HelloController.java
@@ -0,0 +1,14 @@
+package group2.monopoly.frontend;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module group2.monopoly.frontend {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens group2.monopoly.frontend to javafx.fxml;
+    exports group2.monopoly.frontend;
+}
