ea5ba6993e37645cfd78a09b14cadf412309b9cf
alpylmz <ylmz.alp.e@gmail.com>
2022-05-25 19:53:26 +0300

    feat(signup): sign up curl request implemented

--- src/main/java/group2/monopoly/frontend/HelloApplication.java
+++ src/main/java/group2/monopoly/frontend/HelloApplication.java
@@ -11,13 +11,22 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import group2.monopoly.frontend.Pawn.Pawn;
 
+@SpringBootApplication
+@RestController
+@RequestMapping("/frontend")
 public class HelloApplication extends Application {
     private final int grid_count = 7;
     private boolean in_game = false;
@@ -66,7 +75,49 @@ public class HelloApplication extends Application {
         this.scene.setRoot(RenderSignUp.render(this, width, height));
     }
 
+    public void executeSignUp(String username, String password, String confirmPassword, String email) throws IOException {
+        URL url = new URL("http://localhost:8080/api/auth/register");
+        /*
+        String postData = "username=" + username + "&" +
+                "email=" + email + "&" +
+                "password=" + password + "&" +
+                "confirmPassword=" + confirmPassword;
+        */
+        String postData = "{ \"username\": \"" + username + "\", \"password\": \"" + password + "\", \"confirmPassword\": \"" + confirmPassword + "\", \"email\": \"" + email + "\"}";
+        System.out.println("the post data is");
+        System.out.println(postData);
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setDoOutput(true);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json");
+
+        OutputStream os = conn.getOutputStream();
+        os.write(postData.getBytes());
+        os.flush();
+
+        if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+            throw new RuntimeException("Failed: HTTP error code: " + conn.getResponseCode());
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        String output;
+        System.out.println("Output from server ... \n");
+        while ((output = br.readLine()) != null) {
+            System.out.println(output);
+        }
+
+        conn.disconnect();
+
+    }
+
     public static void main(String[] args) {
         launch();
+    }
+
+    @GetMapping("/startGame")
+    public void startGameRest() {
+        startGame();
     }
 }
