b588b9403ddbfb80649e7530d1b74a2d967ec22b
alpylmz <ylmz.alp.e@gmail.com>
2022-06-20 12:48:18 +0300

    doc(game): add javadoc comments to property display texts

--- src/main/java/group2/monopoly/frontend/CreatePropertyDisplay.java
+++ src/main/java/group2/monopoly/frontend/CreatePropertyDisplay.java
@@ -5,8 +5,19 @@ import org.json.JSONArray;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+  * This class has a static method that translates the owned purchasable indices to the actual screen rectangle coordinates, and returns the texts.
+ */
 public class CreatePropertyDisplay {
 
+    /**
+      * This method returns the texts of the owned purchasable's texts
+      * @param owned_purchasables The indices of the owned purchasables
+      * @param propertyIndices The indices of the properties
+      * @param portIndices The indices of the ports
+      * @param propertyNames The names of the properties
+      * @return List<String> The texts of the owned purchasables
+      */
     public static List<String> findNames (JSONArray ownedPurchasables, JSONArray propertyIndices, JSONArray portIndices, List<String> propertyNames) {
         List<String> result = new ArrayList<String>();
         List<Object> ownedPurchasablesList = ownedPurchasables.toList();
